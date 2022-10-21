39b205fdeb2634a09ab37580a0da28543daf63ed
Abdullah <A21alharbi9@gmail.com>
2022-10-21 16:37:19 +0300

    Added sign up, sign in forms and db connection

---
+++ public/signUp.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home | hospital</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <label for="fullName"> Full name: </label>
+        <input type="text" name="fullName" placeholder="Your full name" />
+        <label for="email"> Email: </label>
+        <input type="email" name="email" placeholder="Your email" />
+        <label for="password"> Password: </label>
+        <input type="password" name="password" placeholder="Your password" />
+        <label for="confirmPassword"> Confirm Password: </label>
+        <input type="password" name="confirmPassword" placeholder="Your confirm password" />
+        <button type="submit" name="signupBtn">Sign up</button>
+    </form>
+
+</body>
+
+</html>
