2c29ee36ff6c32ff817a3cedec264ac325fbaa14
Abdullah <A21alharbi9@gmail.com>
2022-12-21 08:37:52 +0300

    Added Sign Up and Sign In pages styles

--- public/signIn.php
+++ public/signIn.php
@@ -18,7 +18,7 @@ require_once "../src/config/functions.global.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/signin.css" />
+    <link rel="stylesheet" href="./styles/signIn.css" />
     <title><?php pageTitle('sign in') ?></title>
 </head>
 
@@ -26,40 +26,67 @@ require_once "../src/config/functions.global.php";
 
     <?php include_once("./includes/nav.php") ?>
 
-    <div class="selectionContainer" onclick="activeEle(event)">
-        <button class="doctorBtn">
-            <p>Doctor</p>
-        </button>
-        <button class="patientBtn active">Patient</button>
+    <div class="section_wrapper">
+
+        <div class="form_wrapper">
+
+            <div class="form_img_wrapper">
+                <img src="https://images.unsplash.com/photo-1576502200272-341a4b8d5ebb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
+            </div>
+
+            <div class="form_selection_wrapper">
+
+                <div class="selectionContainer" onclick="activeEle(event)">
+                    <button class="doctor_btn">
+                        <p>Doctor</p>
+                    </button>
+                    <button class="patient_btn active">Patient</button>
+                </div>
+
+
+                <div class="patientForm">
+                    <form action="../src/auth/signin.php" method="post">
+                        <input type="hidden" name="role" value="patient" />
+                        <div class="form_element">
+                            <label for="email"> Email: </label>
+                            <input type="email" name="email" placeholder="Your email" required />
+                        </div>
+
+                        <div class="form_element">
+                            <label for="password"> Password: </label>
+                            <input type="password" name="password" placeholder="Your password" required />
+                        </div>
+
+                        <div class="form_button_wrapper">
+                            <button type="submit" name="signinBtn">Sign in</button>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="doctorForm">
+                    <form action="../src/auth/signin.php" method="post">
+
+                        <input type="hidden" name="role" value="doctor" />
+
+
+                        <div class="form_element">
+                            <label for="email"> Email: </label>
+                            <input type="email" name="email" placeholder="Your email" required />
+                        </div>
+
+                        <div class="form_element">
+                            <label for="password"> Password: </label>
+                            <input type="password" name="password" placeholder="Your password" required />
+                        </div>
+
+                        <div class="form_button_wrapper">
+                            <button type="submit" name="signinBtn">Sign in</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-
-
-    <div class="patientForm">
-        <form action="../src/auth/signin.php" method="post">
-
-            <input type="hidden" name="role" value="patient" />
-            <label for="email"> Email: </label>
-            <input type="email" name="email" placeholder="Your email" required />
-            <label for="password"> Password: </label>
-            <input type="password" name="password" placeholder="Your password" required />
-
-            <button type="submit" name="signinBtn">Sign in</button>
-
-        </form>
-    </div>
-
-    <div class="doctorForm">
-        <form action="../src/auth/signin.php" method="post">
-
-            <input type="hidden" name="role" value="doctor" />
-            <label for="email"> Email: </label>
-            <input type="email" name="email" placeholder="Your email" required />
-            <label for="password"> Password: </label>
-            <input type="password" name="password" placeholder="Your password" required />
-
-            <button type="submit" name="signinBtn">Sign in</button>
-
-        </form>
     </div>
 
     <?php include_once("./includes/footer.php") ?>
@@ -71,11 +98,11 @@ require_once "../src/config/functions.global.php";
 
         let ele = event.target;
 
-        let eleDoc = document.querySelector('.doctorBtn');
+        let eleDoc = document.querySelector('.doctor_btn');
 
         let eleDocForm = document.querySelector('.doctorForm');
 
-        let elePat = document.querySelector('.patientBtn');
+        let elePat = document.querySelector('.patient_btn');
 
         let elePatForm = document.querySelector('.patientForm');
 
@@ -83,14 +110,20 @@ require_once "../src/config/functions.global.php";
         if (ele.innerText.toLowerCase() === 'doctor') {
 
             eleDoc.classList.add("active");
-            eleDocForm.style.display = "block"
+            eleDocForm.style.display = "flex"
+            eleDocForm.style.flexDirection = "column"
+            eleDocForm.style.justifyContent = "center"
+            eleDocForm.style.alignItems = "center"
 
             elePat.classList.remove("active");
             elePatForm.style.display = "none"
         } else {
 
             elePat.classList.add("active");
-            elePatForm.style.display = "block"
+            elePatForm.style.display = "flex"
+            elePatForm.style.flexDirection = "column"
+            elePatForm.style.justifyContent = "center"
+            elePatForm.style.alignItems = "center"
 
             eleDoc.classList.remove("active");
             eleDocForm.style.display = "none"
--- public/signUp.php
+++ public/signUp.php
@@ -14,6 +14,7 @@ if (!isset($_SESSION)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/signUp.css" />
     <title><?php pageTitle('sign up') ?></title>
 </head>
 
@@ -21,25 +22,50 @@ if (!isset($_SESSION)) {
 
     <?php include_once("./includes/nav.php") ?>
 
-    <form action="../src/auth/signup.php" method="post">
+    <div class="section_wrapper">
 
-        <label for="role">Are you a</label>
+        <div class="form_wrapper">
+            <div class="form_img_wrapper">
+                <img src="https://images.unsplash.com/photo-1576502200916-3808e07386a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2065&q=80" />
+            </div>
 
-        <select name="role">
-            <option value="patient">patient</option>
-            <option value="doctor">doctor</option>
-        </select>
+            <form action="../src/auth/signup.php" method="post">
 
-        <label for="fullName"> Full name: </label>
-        <input type="text" name="fullName" placeholder="Your full name" />
-        <label for="email"> Email: </label>
-        <input type="email" name="email" placeholder="Your email" />
-        <label for="password"> Password: </label>
-        <input type="password" name="password" placeholder="Your password" />
-        <label for="confirmPassword"> Confirm Password: </label>
-        <input type="password" name="confirmPassword" placeholder="Your confirm password" />
-        <button type="submit" name="signupBtn">Sign up</button>
-    </form>
+                <div class="form_element">
+                    <label for="role">Are you a</label>
+
+                    <select name="role">
+                        <option value="patient">patient</option>
+                        <option value="doctor">doctor</option>
+                    </select>
+                </div>
+
+                <div class="form_element">
+                    <label for="fullName"> Full name: </label>
+                    <input type="text" name="fullName" placeholder="Your full name" />
+                </div>
+
+                <div class="form_element">
+                    <label for="email"> Email: </label>
+                    <input type="email" name="email" placeholder="Your email" />
+                </div>
+
+                <div class="form_element">
+                    <label for="password"> Password: </label>
+                    <input type="password" name="password" placeholder="Your password" />
+                </div>
+
+                <div class="form_element">
+                    <label for="confirmPassword"> Confirm Password: </label>
+                    <input type="password" name="confirmPassword" placeholder="Your confirm password" />
+                </div>
+
+                <div class="form_button_wrapper">
+                    <button type="submit" name="signupBtn">Sign up</button>
+                </div>
+            </form>
+        </div>
+    </div>
 
     <?php include_once("./includes/footer.php") ?>
 
