3a1ff79b3b27f1239a89c7b696e2ff3e0893a75b
Abdullah <A21alharbi9@gmail.com>
2023-01-14 16:55:14 +0300

    Changed Nav and added features to Dashboard

--- public/dashboard.php
+++ public/dashboard.php
@@ -42,114 +42,233 @@ if (isset($_POST['addDeptBtn'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php pageTitle('dashboard') ?></title>
+    <link rel="stylesheet" href="./styles/dashboard.css" />
 </head>
 
 <body>
 
     <?php include_once("./includes/nav.php") ?>
 
+
     <h1>Dashboard</h1>
-    <?php
 
-    if (isset($_SESSION['fullName'])) {
-    ?>
-        <h2><?= $_SESSION['table'] === 'doctorData' ? "Welcome Dr. $_SESSION[fullName]" : "Welcome, $_SESSION[fullName]" ?> </h2>
-
+    <div class="dashboard_wrapper">
         <?php
 
-        if ($_SESSION['table'] === 'doctorData') {
-
+        if (isset($_SESSION['fullName'])) {
         ?>
+            <h2><?= $_SESSION['table'] === 'doctorData' ? "Welcome Dr. $_SESSION[fullName]" : "Welcome, $_SESSION[fullName]" ?> </h2>
 
             <?php
 
-            $user = new Db();
-
-            $checkDepartment = $user->createConnection()->query("SELECT * FROM doctorData where id=$_SESSION[id] AND department_Id < 1", PDO::FETCH_ASSOC);
-
-
-            if ($checkDepartment->rowCount() > 0) {
+            if ($_SESSION['table'] === 'doctorData') {
 
             ?>
+                <?php
 
-                <form method='post'>
+                $user = new Db();
 
-                    <label for='department'> Choose your department: </label>
-                    <select name='department' required>
-                        <option value=''></option>
-
-                        <?php
+                $checkDepartment = $user->createConnection()->query("SELECT * FROM doctorData where id=$_SESSION[id] AND department_Id < 1", PDO::FETCH_ASSOC);
 
 
-                        if (file_exists(dirname(__File__) . "../../src/cache/dashboard.txt")) {
+                if ($checkDepartment->rowCount() > 0) {
 
-                            $file = unserialize(file_get_contents(dirname(__File__) . "../../src/cache/dashboard.txt"));
+                ?>
 
-                            for ($i = 0; $i < count($file); $i++) {
+                    <form method='post'>
 
-                                echo  "<option value=" . $file[$i]['department']['id'] . ">" . (string) $file[$i]['department']['name'] . "</option>";
-                            }
-                        } else {
+                        <label for='department'> Choose your department: </label>
+                        <select name='department' required>
+                            <option value=''></option>
 
-                            $fetchData = new Db();
+                            <?php
 
-                            $data = $fetchData->createConnection()->query("SELECT * FROM departments", PDO::FETCH_ASSOC);
 
-                            $_SESSION["departments"] = array();
+                            if (file_exists(dirname(__File__) . "../../src/cache/dashboard.txt")) {
 
-                            if ($data->rowCount() > 0) {
+                                $file = unserialize(file_get_contents(dirname(__File__) . "../../src/cache/dashboard.txt"));
 
-                                $i = 0;
+                                for ($i = 0; $i < count($file); $i++) {
 
-                                foreach ($data as $row) {
-
-                                    if (in_array($row['departmentName'], $_SESSION['departments']) == false) {
-
-                                        $_SESSION["departments"][$i] = [
-                                            "department" => [
-                                                'id' => $row['id'],
-                                                'name' => $row['departmentName']
-                                            ]
-                                        ];
-
-                                        $i++;
-                                    }
-
-                                    echo  "<option value=$row[id]>$row[departmentName]</option>";
+                                    echo  "<option value=" . $file[$i]['department']['id'] . ">" . (string) $file[$i]['department']['name'] . "</option>";
                                 }
+                            } else {
+
+                                $fetchData = new Db();
+
+                                $data = $fetchData->createConnection()->query("SELECT * FROM departments", PDO::FETCH_ASSOC);
+
+                                $_SESSION["departments"] = array();
+
+                                if ($data->rowCount() > 0) {
+
+                                    $i = 0;
+
+                                    foreach ($data as $row) {
+
+                                        if (in_array($row['departmentName'], $_SESSION['departments']) == false) {
+
+                                            $_SESSION["departments"][$i] = [
+                                                "department" => [
+                                                    'id' => $row['id'],
+                                                    'name' => $row['departmentName']
+                                                ]
+                                            ];
+
+                                            $i++;
+                                        }
+
+                                        echo  "<option value=$row[id]>$row[departmentName]</option>";
+                                    }
+                                }
+
+                                file_put_contents(dirname(__File__) . "../../src/cache/dashboard.txt", serialize($_SESSION["departments"]));
                             }
+                            ?>
+                        </select>
 
-                            file_put_contents(dirname(__File__) . "../../src/cache/dashboard.txt", serialize($_SESSION["departments"]));
-                        }
-                        ?>
-                    </select>
+                        <button type='submit' name='addDeptBtn' class='addDeptBtn'>Add</button>
 
-                    <button type='submit' name='addDeptBtn' class='addDeptBtn'>Add</button>
+                    </form>
 
-                </form>
+                <?php
+                } else {
+                ?>
+                    <div class="doctor_dashboard_wrapper">
 
-            <?php
+                        <div class="patients_app_wrapper">
+                            <div class="patients_number_wrapper">
+                                <?php
+                                $fetchAppointments = new Db();
+
+                                $data = $fetchAppointments->createConnection()->query("SELECT * FROM appointments WHERE DATE(appointmentDate) <= DATE(UTC_TIMESTAMP() + INTERVAL 1 WEEK)", PDO::FETCH_ASSOC);
+
+                                if ($data->rowCount() > 0) {
+
+                                    echo "<span class='patients_number'>" . $data->rowCount() . "</span>";
+                                } else {
+                                    echo "<span class='patients_number'>0</span>";
+                                }
+                                ?>
+                                <p>patient(s) this week</p>
+                            </div>
+
+                            <div class="next_app_wrapper">
+                                <p class='next_app_main'>Upcoming appointment</p>
+                                <?php
+
+                                $appRow = $data->fetch(PDO::FETCH_ASSOC);
+
+
+                                if ($data->rowCount() > 0) {
+
+                                    echo  "<p class='next_app_date'>" . $appRow['appointmentDate'] . "</p>";
+                                } else {
+
+                                    echo "<p class='next_app_date_clear'>No appointments</p>";
+                                }
+
+
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="search_app_wrapper">
+                        <input type="text" name="appSearchInput" class="appSearchInput" placeholder="search appointments" onkeyup="filterAppointments()">
+                        <div class="app_table_wrapper">
+                            <?php
+
+                            $patientData = $fetchAppointments->createConnection()->query("SELECT * FROM appointments", PDO::FETCH_ASSOC);
+
+                            if ($patientData->rowCount() > 0) {
+
+                            ?>
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <td>
+                                                Patient id
+                                            </td>
+                                            <td>
+                                                App. date
+                                            </td>
+                                            <td>
+                                                Department
+                                            </td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                        <?php foreach ($patientData as $row) { ?>
+
+                                            <tr class="table_data_row">
+                                                <td><?php echo $row['patient_id']; ?>
+                                                </td>
+                                                <td><?php echo $row['appointmentDate']; ?>
+                                                </td>
+                                                <td><?php echo $row['department_id']; ?>
+                                                </td>
+                                            </tr>
+
+                                        <?php } ?>
+
+                                    </tbody>
+                                </table>
+                            <?php
+                            } else {
+                            ?>
+                                <h1>No appointments</h1>
+                            <?php
+                            }
+                            ?>
+                        </div>
+                    </div>
+
+                    <!-- <div class="toggle_wrapper">
+                        <div class="toggle_btn_wrapper">
+                            <button class="toggle_a">A</button>
+                            
+                            <button class="toggle_b">B</button>
+                        </div> -->
+    </div>
+
+<?php
+                }
             } else {
-            ?>
+?>
 
-                <h3>All done :)</h3>
-
-            <?php
+<h3>Have a nice day!</h3>
+<?php
             }
         } else {
-            ?>
-
-            <h3>Have a nice day!</h3>
-
-    <?php
+            echo "<h1>Sign in to view this page!</h1>";
         }
-    } else {
-        echo "<h1>Sign in to view this page!</h1>";
+?>
+</div>
+
+<?php include_once("./includes/footer.php") ?>
+</div>
+<script>
+    function filterAppointments() {
+        const inputTxt = document.querySelector('.appSearchInput').value.toUpperCase();
+
+        const allRows = document.querySelectorAll('.table_data_row');
+
+        for (let index = 0; index < allRows.length; index++) {
+
+
+            if (allRows[index].innerText.toUpperCase().includes(inputTxt)) {
+
+                allRows[index].style.display = 'table-row';
+
+            } else {
+
+                allRows[index].style.display = 'none';
+            }
+        }
     }
-    ?>
-
-    <?php include_once("./includes/footer.php") ?>
-
+</script>
 </body>
 
 </html>
--- public/includes/nav.php
+++ public/includes/nav.php
@@ -18,15 +18,29 @@ require_once "../src/config/functions.global.php";
         }
         ?>
     </ul>
-    <div>
-        <button class="nav_btn_secondary">
-            <a href="../public/signIn.php">
-                Sign in
-            </a>
-        </button>
-        <button class="nav_btn_main">
-            <a href="../public/signUp.php">
-                Sign up</a>
-        </button>
-    </div>
+    <?=
+    isset($_SESSION['fullName']) ?  "
+            <div class='nav_auth_wrapper'>
+                <button class='nav_btn_auth_main'>
+                    <a href='../public/dashboard.php'>
+                    " . $_SESSION['fullName'] . "
+                    </a>
+                </button> 
+            </div>
+            "
+        :  "
+            <div>
+                <button class='nav_btn_secondary'>
+                    <a href='../public/signIn.php'>
+                        Sign in
+                    </a>
+                </button>
+                <button class='nav_btn_main'>
+                    <a href='../public/signUp.php'>
+                        Sign up
+                    </a>
+                </button>
+            </div>
+                "
+    ?>
 </nav>
